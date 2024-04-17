7429fcc7f21b68113aaf305d104d7e9303b6c6d7
deepeshahirwar <deepeshahirwar7024@gmail.com>
2024-04-17 19:58:51 +0530

    updated form

--- 02Form/src/App.jsx
+++ 02Form/src/App.jsx
@@ -1,13 +1,20 @@
 import React, { useState } from 'react';
 
-import './App.css';
+import './App.css'; 
+import { useForm, ValidationError } from '@formspree/react';
 
-function App() {
+function ContactForm(){
+  const [state, handleSubmit] = useForm("xwkgakdb");
+  if (state.succeeded) {
+      return <p>Thanks for joining!</p>;
+  }
+
+
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [msg, setMsg] = useState("");
 
-  const handleSubmit = (event) => {
+  const handleSubmitForm = (event) => {
     event.preventDefault();
     const newName = event.target[0].value;
     const newEmail = event.target[1].value;
@@ -20,27 +27,53 @@ function App() {
 
   return (
     <>
-      <h1 className='text-center text-3xl mt-5 text-white font-bold'>Contact me</h1>
+      {/* <h1 className='text-center text-3xl mt-5 text-white font-bold'>Contact me</h1> */}
       <div className="container flex justify-center">
-        <form className="flex flex-col justify-center" onSubmit={handleSubmit}>
+        <form 
+        action="https://formspree.io/f/xwkgakdb"
+        method="POST"
+        className="flex flex-col justify-center" onSubmit={handleSubmitForm}>
           <input
             type="text"
+            name="username"
             placeholder='Enter your name'
             className='mt-5 w-[30rem] h-[3rem] px-2 text-xl text-gray-400 border-solid border-2 border-sky-500 bg-slate-700 rounded-md'
           />
+
+          <ValidationError 
+           prefix="Email" 
+          field="email"
+           errors={state.errors}
+           />
+
           <input
-            type="text"
+            type="email" 
+            name="Email"
             placeholder='Enter your email'
             className='mt-5 w-[30rem] h-[3rem] px-2 text-xl text-gray-400 bg-slate-700 border-solid border-2 border-sky-500 rounded-md'
-          />
-          <textarea
+          /> 
+
+      <ValidationError 
+        prefix="Email" 
+        field="email"
+        errors={state.errors}
+        />
+          <textarea 
+            name='message'
             cols="30"
             rows="10"
             className='mt-5 px-2 text-xl text-gray-400 bg-slate-700 border-solid border-2 border-sky-500 rounded-md'
             placeholder='Write your comment here...'
-          ></textarea>
+          ></textarea> 
+
+       <ValidationError 
+        prefix="Message" 
+        field="message"
+          errors={state.errors}
+        />
           <button
-            type="submit"
+            type="submit" 
+            value="send"
             className='border mt-10 bg-white text-black w-[30rem] h-[2rem] font-bold rounded-md'
           >
             Submit
@@ -57,6 +90,17 @@ function App() {
       </div>
     </>
   );
+} 
+
+
+
+
+function App(){
+  return(
+    <> 
+    <ContactForm/>
+    </>
+  )
 }
 
 export default App;
